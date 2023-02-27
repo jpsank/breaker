7a0a16a4cb3ee0cf0e9ee260be53728b261f7b95
Julian Sanker <julian@sankergroup.org>
2023-02-27 08:38:28 -0500

    add join and filter plus dupl dropping for search result analysis

--- parse.py
+++ parse.py
@@ -3,20 +3,29 @@ from collections import defaultdict
 import pandas
 from pyfaidx import Fasta
 
+
 @dataclass
 class Sequence:
     name: str
     start: int
     end: int
-    seq: str
-    strand: str = '+'  # + = forward, - = reverse
+    strand: str
+    seq: str = None
 
     @property
     def coords(self):
-        return (self.start, self.end) if self.strand == 1 else (self.end, self.start)
+        return (self.start, self.end)
+
+    @property
+    def eslcoords(self):
+        return (self.start, self.end) if self.strand == '+' else (self.end, self.start)
+
+    @property
+    def esltag(self):
+        return f"{self.name}/{self.start}-{self.end}"
 
     @staticmethod
-    def from_line(line: str):
+    def from_sto_line(line: str):
         # Split into name and sequence
         parts = line.split(maxsplit=1)
         if len(parts) == 1:
@@ -42,8 +51,7 @@ class Sequence:
             seq = seq[::-1]
             strand = '-'
         
-        # Add sequence to dict
-        return Sequence(name, start, end, seq, strand)
+        return Sequence(name, start, end, strand, seq)
 
 
 @dataclass
@@ -73,6 +81,8 @@ class Feature:
 class Stockholm:
     msa: 'dict[str, Sequence]'
     features: 'dict[str, dict[str, list[str]]]'
+
+    path: str = None
     
     SS_cons: str = field(init=False)
     def __post_init__(self):
@@ -89,7 +99,7 @@ class Stockholm:
                     if feat.name not in features[feat.category]: features[feat.category][feat.name] = []
                     features[feat.category][feat.name].append(feat)
             elif line and line != '//':  # Sequence
-                seq = Sequence.from_line(line)
+                seq = Sequence.from_sto_line(line)
                 msa[seq.name] = seq
         return cls(msa, features, **kwargs)
 
@@ -97,101 +107,97 @@ class Stockholm:
     def from_file(cls, path: str, **kwargs):
         with open(path) as f:
             data = f.read()
-        return cls.from_data(data, **kwargs)
+        return cls.from_data(data, path=path, **kwargs)
 
 
 @dataclass
-class SearchResultHit:
-    target_name: str
-    target_accession: str
-    query_name: str
-    query_accession: str
-    mdl: str
-    mdl_from: int
-    mdl_to: int
-    seq_from: int
-    seq_to: int
-    strand: str
-    trunc: str
-    pass_: str
-    gc: float
-    bias: float
-    score: float
-    E_value: float
-    inc: int
-    description_of_target: str
+class SearchResultHit(Sequence):
+    score: float = None
+    E_value: float = None
+    inc: str = None
+    # duplicates: 'set[SearchResultHit]' = field(default_factory=set)
 
     @staticmethod
-    def from_row(row: pandas.Series):
-        return SearchResultHit(
-            target_name=row['target_name'],
-            target_accession=row['target_accession'],
-            query_name=row['query_name'],
-            query_accession=row['query_accession'],
-            mdl=row['mdl'],
-            mdl_from=row['mdl_from'],
-            mdl_to=row['mdl_to'],
-            seq_from=row['seq_from'],
-            seq_to=row['seq_to'],
-            strand=row['strand'],
-            trunc=row['trunc'],
-            pass_=row['pass'],
-            gc=row['gc'],
-            bias=row['bias'],
-            score=row['score'],
-            E_value=row['E-value'],
-            inc=row['inc'],
-            description_of_target=row['description_of_target'],
-        )
+    def from_tblrow_and_sto(row: pandas.Series, sto: Stockholm):
+        # TODO: row['strand'] is '' or nan when it should be '+'
+        kwargs = {
+            'name': row['target_name'].strip(),
+            'start': int(row['seq_from']),
+            'end': int(row['seq_to']),
+            'strand': row['strand'],
+            'score': float(row['score']),
+            'E_value': float(row['E_value']),
+            'inc': row['inc'].strip(),
+        }
+        if kwargs['strand'] == '-':
+            kwargs['start'], kwargs['end'] = kwargs['end'], kwargs['start']
+        
+        if kwargs['inc'] == '!':
+            seq = sto.msa[kwargs['name']]
+            kwargs.update(seq.__dict__)
+        return SearchResultHit(**kwargs)
+
 
 @dataclass
 class SearchResult:
-    sto: Stockholm = field(init=False)
-    tbl: 'dict[str, SearchResultHit]'
-
+    sto: Stockholm
+    hits: 'dict[str, SearchResultHit]'
+    tbl_path: str = None
     dbfna: str = None
-    fasta: Fasta = field(init=False)
+    fasta: Fasta = None
 
     def __post_init__(self):
-        self.fasta = Fasta(self.dbfna)
+        if self.dbfna:
+            self.fasta = Fasta(self.dbfna)
+        # self.check_duplicates()
 
     @staticmethod
     def from_files(sto_path: str, tbl_path: str, dbfna: str = None):
-        sr = SearchResult(dbfna=dbfna)
-        # Read sto
-        sr.sto = Stockholm.from_file(sto_path)
+        sto = Stockholm.from_file(sto_path)
 
-        # Read tbl
-        colnames = ['target_name', 'target_accession', 'query_name', 'query_accession', 'mdl', 'mdl_from', 'mdl_to', 'seq_from', 'seq_to', 'strand', 'trunc', 'pass', 'gc', 'bias', 'score', 'E-value', 'inc', 'description_of_target']
-        df = pandas.read_fwf(tbl_path, comment='#', delim_whitespace=True, on_bad_lines='warn', names=colnames)
-    
-        # Join tbl with sto
+        colnames = ['target_name', 'target_accession', 'query_name', 'query_accession', 'mdl', 'mdl_from', 'mdl_to', 'seq_from', 'seq_to', 'strand', 'trunc', 'pass', 'gc', 'bias', 'score', 'E_value', 'inc', 'description_of_target']
+        hits = {}
+        df = pandas.read_fwf(tbl_path, comment='#', delim_whitespace=True, on_bad_lines='warn', header=None, names=colnames, index_col=False, dtype=str, na_filter=False)
         for i, row in df.iterrows():
-            seq = sr.sto.msa.get(row['target_name'])
-            hit = SearchResultHit.from_(row)
-            key = f"{hit.target_name}/{hit.seq_from}-{hit.seq_to}"
-            sr.tbl[key] = hit
+            hit = SearchResultHit.from_tblrow_and_sto(row, sto)
+            hits[f"{hit.name}/{hit.start}-{hit.end}"] = hit
 
-        return sr
+        return SearchResult(sto, hits, tbl_path=tbl_path, dbfna=dbfna)
+
+    # def check_duplicates(self):
+    #     for hit in self.hits.values():
+    #         if hit.inc != '!':
+    #             continue
+    #         for other in self.hits.values():
+    #             if hit is other or other.inc != '!':
+    #                 continue
+    #             if hit.seq == other.seq:
+    #                 print(f"Duplicate hit: {hit.name}/{hit.start}-{hit.end} and {other.name}/{other.start}-{other.end}")
+    #                 hit.duplicates.add(other.esltag)
+    #                 other.duplicates.add(hit.esltag)
 
     def get_flank(self, seq: Sequence, flank: int):
+        if not self.fasta:
+            raise ValueError("No database FASTA provided")
         # TODO: Bounds checking
         s = self.fasta[seq.name][seq.start-1-flank:seq.end+flank]
         if seq.strand == -1:
             s = s.complement
         return Sequence(seq.name, seq.start-flank, seq.end+flank, str(s), seq.strand)
 
+    def to_dataframe(self):
+        return pandas.DataFrame([h.__dict__ for h in self.hits.values()])
+
 
 def main():
     # sto = parse_file('../search/sto/DUF1646/RF03071.sto')
     sr = SearchResult.from_files(
-        '../search/searches/bacteria-DUF1646-6/bacteria-DUF1646-6.out.sto',
-        '../search/searches/bacteria-DUF1646-6/bacteria-DUF1646-6.out.tbl',
-        dbfna='/Users/julian/bioinfo/db/rs213-bact-repr/rs213-bact-repr.fna')
+        './search/searches/gtdb-bact_DUF1646_1/gtdb-bact_DUF1646_1.out.sto',
+        './search/searches/gtdb-bact_DUF1646_1/gtdb-bact_DUF1646_1.out.tbl',
+    )
+        # dbfna='/Users/julian/bioinfo/db/rs213-bact-repr/rs213-bact-repr.fna')
     
-    # print(sr.msa)
-    # seq = sr.msa['NZ_QFFZ01000015.1']
-    seq = sr.sto.msa['NZ_BJDX01000003.1']
+    seq = sr.sto.msa['NZ_AUUU01000100.1']
     flanked = sr.get_flank(seq, 0)
     print(seq)
     print(flanked)
