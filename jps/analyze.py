77f57dd6beadcf488e4d1b3991eaa76ae73733fc
Julian Sanker <julian@sankergroup.org>
2023-03-06 13:58:20 -0500

    integrate compare into analyze.py

--- jps/analyze.py
+++ jps/analyze.py
@@ -10,6 +10,31 @@ import numpy as np
 from jps.inferno import *
 
 
+def compare(sr1: SearchResult, sr2: SearchResult, distance_threshold=1000):
+    # Get target names that are in both sr1 and sr2
+    hits1 = defaultdict(list)
+    for h in sr1.hits.values():
+        hits1[h.row.target_name].append(h)
+    hits2 = defaultdict(list)
+    for h in sr2.hits.values():
+        hits2[h.row.target_name].append(h)
+    targets = set(hits1.keys()).intersection(set(hits2.keys()))
+    
+    # Check for intersecting hits
+    for target_name in targets:
+        if target_name in hits1 and target_name in hits2:
+            for h1 in hits1[target_name]:
+                for h2 in hits2[target_name]:
+                    from1, to1 = h1.row.seq_from, h1.row.seq_to
+                    from2, to2 = h2.row.seq_from, h2.row.seq_to
+                    if from1 > to1: from1, to1 = to1, from1
+                    if from2 > to2: from2, to2 = to2, from2
+                    if abs(from1-from2) < distance_threshold or abs(to1-to2) < distance_threshold:
+                        print(f"compare: Potential intersect at {target_name}, 1:{from1}-{to1} 2:{from2}-{to2}")
+    else:
+        print("compare: No intersecting hits found")
+
+
 def analyze(sr: SearchResult, name, color, outdir='search/analyze', threshold=0.01):
     # Table of counts/statistics
     print(f"Total number of {name} hits: {len(sr.hits)}")
